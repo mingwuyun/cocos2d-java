b2e3135a7c629d54f89c85aaa6fb5925fe5446af
mingwuyun <mingwuyun2@aliyun.com>
2017-03-01 16:16:52 +0800

    CC.File

--- src/com/cocos2dj/macros/CC.java
+++ src/com/cocos2dj/macros/CC.java
@@ -1,7 +1,9 @@
 package com.cocos2dj.macros;
 
+import com.badlogic.gdx.files.FileHandle;
 import com.cocos2dj.base.Director;
 import com.cocos2dj.module.Module;
+import com.cocos2dj.platform.FileUtils;
 import com.cocos2dj.renderer.Texture;
 import com.cocos2dj.s2d.Scene;
 
@@ -20,5 +22,10 @@ public final class CC {
 	public static Texture LoadImage(String fileName) {
 		Texture t = Director.getInstance().getTextureCache().addImage(fileName);
 		return t;
+	}
+	
+	//file
+	public static FileHandle File(String fileName) {
+		return FileUtils.getInstance().getFileHandle(fileName);
 	}
 }
